6f8495b65d74f95631e70fe4bce190273675db30
kzoltan99 <kzotya99@gmail.com>
2019-02-21 16:23:15 +0100

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <omp.h>
+main()
+{
+    #pragma omp parallel
+    while(1){}
+}
---
+++ loop0.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+main()
+{
+    while(1){}
+            sleep(1);
+
+}
